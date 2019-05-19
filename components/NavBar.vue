41d7e8d8d73b7ca42a9ee51b3c8fde6452fe3989
Jon <jon.howell314@gmail.com>
2019-05-19 21:55:38 +0800

    nav bar overlay. Changes router path and highlights correctly

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,16 +1,29 @@
 <template>
-  <div id="z2-nav-bar" v-if="show" :style="heightStyle">
-    <div id="nav-bar-list">
-      {{heightStyle}}
-      <router-link
-        class="nav-bar-item"
-        v-for="route in routeLinks"
-        :key="route.path"
-        :to="route.path"
-      >
-        {{route.name}}
-      </router-link>
-    </div>
+  <div id="z2-nav-bar">
+    <transition name="modal">
+      <div class="modal-mask" @click.stop="closeNavBar">
+        <div class="modal-container">
+          <div 
+            v-for="route in routeLinks" 
+            :key="route.path"
+            class="nav-item" 
+            :class="showHighlight(route)" 
+            @click.capture="select(route)"
+          >
+            * {{route.name}}
+          </div>
+          <!-- <route-link 
+            v-for="route in routes"
+            :key="route.path" 
+            class="nav-item" 
+            :class="showHighlight(route)"
+            :to="route.path"
+            @click="select(route)">
+              {{route.name}}
+          </route-link> -->
+        </div> 
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -19,44 +32,116 @@ export default {
   name: 'z2NavBar',
   props: {
     routes: { type: Array, default: () => [] },
-    show: { type: Boolean, default: false },
   },
   data: () => {
     return {
-      showNavBar: false,
+      selectedRoutePath: null,
     };
   },
   computed: {
-    heightStyle() {
-      console.dir('lol')
-      const headerHeight = document.getElementById('z2-header').getBoundingClientRect().height;
-      const footerHeight = document.getElementById('z2-footer').getBoundingClientRect().height;
-      const navHeight = window.innerHeight - headerHeight - footerHeight;
-      return {
-        height: `${navHeight}px`
-      }; 
-    },
     routeLinks() {
-      // grouping will go here when enabled
+      // cascading trees will be calculated here
 
       // remove default * path from routes
       const displayRoutes = this.routes.filter(route => route.path !== '*');
       return displayRoutes;
     },
   },
+  methods: {
+    closeNavBar() {
+      this.$emit('close')
+    },
+    select(route) {
+      this.selectedRoutePath = route.path;
+      this.$emit('select', route);
+      this.$router.push(route.path);
+    },
+    showHighlight(route) {
+      let show = false;
+      if (route.path === this.selectedRoutePath) {
+        show = true;
+      }
+      return {
+        'highlight': show,
+      }
+    }
+  }
 };
 </script>
 
 <style>
-#z2-nav-bar {
-  width: 10rem;
-  height: 100%;
-  outline: magenta 1px solid;
-  float: left;
+.highlight {
+  background-color: cyan;
 }
 
-#nav-bar-list {
-  display: flex;
-  flex-direction: column;
+.nav-item {
+  height: 100%;
+  font-size: 2rem;
+  padding: 0.5rem;
+}
+
+.nav-item:hover {
+  background-color: orange;
+}
+
+
+
+
+
+
+/* ----------------------- modal --------------------------*/
+.modal-mask {
+  box-sizing: border-box;
+  padding: 1rem 1rem;
+
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  background-color: blue;
+  transition: opacity .3s ease;
+  overflow: auto;
+}
+
+.modal-wrapper {
+
+  background-color: purple;
+  width: 100%;
+}
+
+.modal-container {
+  width: 100%;
+  background-color: green;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
 }
 </style>
