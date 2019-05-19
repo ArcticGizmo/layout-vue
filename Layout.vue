2d5c6adcba9de184bbd8647cf60764c0b32861ce
Jon <jon.howell314@gmail.com>
2019-05-19 12:39:01 +0800

    added nav bar to layout

--- Layout.vue
+++ Layout.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="layout">
     <z2-header/>
+    <z2-nav-bar :routes="routes"/>
     <z2-footer/>
   </div>
 </template>
@@ -8,15 +9,17 @@
 <script>
 import z2Header from './components/Header.vue';
 import z2Footer from './components/Footer.vue';
+import z2NavBar from './components/NavBar.vue';
 
 export default {
   title: "z2Layout",
   components: {
     z2Header,
     z2Footer,
+    z2NavBar,
   },
   props: {
-
+    routes: { type: Array, default: () => [] },
   },
   methods: {
     method1() {
